99c6fc9870ab7a48ad43c1f73c20b246969a17c8
kongzhou <f195hao@gmail.com>
2020-12-26 13:52:58 +0800

    使用vuepress构建官网存在组件显示问题

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  title: '咕噜UI',
+  description: 'Just playing',
+  themeConfig: {
+    sidebar: [
+      {
+        title: '快速开始',
+        children: [
+          '/install/',
+          '/get_started/'
+        ]
+      },
+      {
+        title: '组件',
+        children: [
+          '/components/button',
+        ]
+      }
+    ]
+  }
+}
---
+++ src/button.vue
@@ -0,0 +1,31 @@
+<template>
+    <button class="g-button">
+        <slot></slot>
+    </button>
+</template>
+
+<script>
+export default {
+  name:'g-button'
+}
+</script>
+
+<style lang="scss" scoped>
+.g-button{
+    font-size:14px;
+    height:32px;
+    padding: 0 1em;
+    border-radius: 4px;
+    border:1px solid ;
+    background: white;
+    &:hover{
+        border-color: #999999;
+    }
+    &:active{
+        background-color: transparent;
+    }
+    &:focus{
+        outline: none;
+    }
+}
+</style>
